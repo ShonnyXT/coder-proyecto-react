62e3eee631eea95bd046a06c27470072b6339b37
Jonatan <jonny.m722@gmail.com>
2023-01-30 15:34:46 -0300

    continuar itemListContainer

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,17 @@
 
+const task = new Promise((resuelto, rechazado)=>{
+  //acciones
+  if (true){
+    return resuelto('Todo ok gracias por la plata')
+  }
+  return rechazado('No puedo devolverte la plata')
+})
+
 const ItemListContainer = ({saludo}) => {
+
+  task
+    .then(resResuelto => console.log(resResuelto), err => console.log(err))
+
   return (
     <div>
         {saludo}
